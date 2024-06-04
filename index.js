5457184c225094486fc5479ef61fabe29328a181
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-05 00:06:12 +0600

    final

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     const teethCareDB = client.db("TeethCareDB");
     const treatmentsCollection = teethCareDB.collection("treatments");
 
